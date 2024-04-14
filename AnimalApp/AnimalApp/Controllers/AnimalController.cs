63241dfe460a535fc8775bfd687e06f363b1554a
spahbod@gmail.com <87122902474Ab!>
2024-04-14 15:23:18 +0200

    Dodanie AnimalController

---
+++ AnimalApp/AnimalApp/Controllers/AnimalController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GakkoHorizontalSlice.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AnimalController : ControllerBase
+{
+    /// <summary>
+    /// Endpoints used to return list of animals.
+    /// </summary>
+    /// <returns>List of animals</returns>
+    [HttpGet]
+    public IActionResult GetAnimals()
+    {
+        return Ok();
+    }
+
+}
