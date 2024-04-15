8e06ae1badfea6d7dceb4de7f549706300cf7286
spahbod@gmail.com <87122902474Ab!>
2024-04-15 15:05:42 +0200

    dodanie walidacji do modelu

--- AnimalApp/AnimalApp/model/Animal.cs
+++ AnimalApp/AnimalApp/model/Animal.cs
@@ -1,11 +1,20 @@
-﻿namespace AnimalApp.model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AnimalApp.model
 {
     public class Animal
     {
+
+        [Required]
         public int IdAnimal { get; set; }
+        [MaxLength(200)]
         public string Name { get; set; }
+        [Required]
+        [MaxLength(200)]
         public string Description { get; set; }
+        [MaxLength(200)]
         public string Category { get; set; }
+        [MaxLength(200)]
         public string Area { get; set; }
     }
 }
