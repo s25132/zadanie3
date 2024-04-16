ca5611b1533ac903663c9af8360e7440e599d39f
spahbod@gmail.com <87122902474Ab!>
2024-04-16 17:56:09 +0200

    dodaj model

---
+++ AnimalApp/AnimalApp/Model/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AnimalApp.Model
+{
+    public class Animal
+    {
+        [MaxLength(200)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(200)]
+        public string Description { get; set; }
+        [MaxLength(200)]
+        public string Category { get; set; }
+        [MaxLength(200)]
+        public string Area { get; set; }
+    }
+}
---
+++ AnimalApp/AnimalApp/Model/AnimalWithId.cs
@@ -0,0 +1,12 @@
+﻿using AnimalApp.Model;
+using System.ComponentModel.DataAnnotations;
+
+namespace AnimalApp.Model
+{
+    public class AnimalWithId : Animal
+    {
+
+        [Required]
+        public int IdAnimal { get; set; }
+    }
+}
